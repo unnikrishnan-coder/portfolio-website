377207567b256712531dbf02ab137c86cabd8f36
unnikrishnan-coder <69650347+unnikrishnan-coder@users.noreply.github.com>
2022-05-26 02:35:52 +0530

    Update NavigationDots.jsx

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -8,7 +8,7 @@ const NavigationDots = ({active}) => {
             key={item+index}
             className="app__navigation-dot"
             style={active === item ? {backgroundColor:'#313bac'}:{}}
-            />
+            > </a>
         ))}
     </div>
   )

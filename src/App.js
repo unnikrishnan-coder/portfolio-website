d82c4e292df720fa1aa95f0438d08af9a1c417c3
unnikrishnan g <unnikrishnang628@gmail.com>
2022-05-26 02:07:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {About,Header,Footer,Work,Skills,Testimonial} from './container';
+import {Navbar} from './components';
+import './App.scss';
+
+const App = () => {
+  return (
+<div className="app">
+  <Navbar />
+  <Header />
+  <About />
+  <Work />
+  <Skills />
+  <Testimonial />
+  <Footer/>
+</div>
+  )
+};
+
+export default App;
